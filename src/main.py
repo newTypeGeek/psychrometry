ffde4fcbb5b07605f39d1c14bc7bd0f62ea7300e
bpht <bennyphtam@gmail.com>
2023-05-26 17:18:53 +0800

    Demo with plotly

---
+++ src/main.py
@@ -0,0 +1,28 @@
+from dash import Dash, dcc, html, Input, Output
+import plotly.graph_objects as go
+
+app = Dash(__name__)
+
+
+app.layout = html.Div(
+    [
+        html.H4("Interactive color selection with simple Dash example"),
+        html.P("Select color:"),
+        dcc.Dropdown(
+            id="dropdown",
+            options=["Gold", "MediumTurquoise", "LightGreen"],
+            value="Gold",
+            clearable=False,
+        ),
+        dcc.Graph(id="graph"),
+    ]
+)
+
+
+@app.callback(Output("graph", "figure"), Input("dropdown", "value"))
+def display_color(color):
+    fig = go.Figure(data=go.Bar(y=[2, 3, 1], marker_color=color))  # replace with your own data source
+    return fig
+
+
+app.run_server(debug=True)
