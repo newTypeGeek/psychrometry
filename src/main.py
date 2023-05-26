2c7c57d8b926a1566c9058e3245116496fc6c540
bpht <bennyphtam@gmail.com>
2023-05-26 19:23:26 +0800

    Impl. dash with iso RH lines

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.graph_objects as go
+import numpy as np
+
+import psy
+
 
 app = Dash(__name__)
 
@@ -18,10 +22,34 @@ app.layout = html.Div(
     ]
 )
 
+HUMIDITY_RATIO_MAX = 0.030
+DRY_BULB_MAX = 60
+
+DRY_BULBS = np.arange(-10, DRY_BULB_MAX, 0.1)
+
+
+def generate_iso_rh_line(rh: float) -> tuple[np.ndarray, np.ndarray]:
+    humidity_ratios = psy.humidity_ratio(DRY_BULBS, rh)
+    is_in_range = humidity_ratios <= HUMIDITY_RATIO_MAX
+    return DRY_BULBS[is_in_range], humidity_ratios[is_in_range]
+
 
 @app.callback(Output("graph", "figure"), Input("dropdown", "value"))
 def display_color(color):
-    fig = go.Figure(data=go.Bar(y=[2, 3, 1], marker_color=color))  # replace with your own data source
+    print("render!")
+    fig = go.Figure()
+
+    for rh in np.arange(10, 100, 10):
+        x, y = generate_iso_rh_line(rh)
+        fig.add_trace(go.Scatter(x=x, y=y, marker_color="gray", name=f"RH = {rh} %"))
+
+    fig.update_layout(
+        autosize=True,
+        height=900,
+        # paper_bgcolor="LightSteelBlue",
+        title=dict(text="Psychrometric Chart", font=dict(size=48)),
+    )
+
     return fig
 
 
