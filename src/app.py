a8ec96f8f073b58af3192e2f726db4184985d30a
bpht <bennyphtam@gmail.com>
2023-05-30 22:10:52 +0800

    Update app

--- src/app.py
+++ src/app.py
@@ -2,16 +2,17 @@ from dash import Dash, dcc, html
 
 import psy_chart
 
-
 app = Dash(__name__)
 app.title = "Psychrometric Chart"
 
+psy_chart_agent = psy_chart.PsyChart()
+figure = psy_chart_agent.render()
 
 app.layout = html.Div(
     [
         html.Title("Psychrometric Chart"),
         html.H1("Dash App", style={"textAlign": "center", "color": "black"}),
-        dcc.Graph(id="graph", figure=psy_chart.create_psy_chart()),
+        dcc.Graph(id="graph", figure=figure),
     ]
 )
 
